8d3d613b07df43521dcfa3cf0cc54769c65a140d
akio0911 <akio0911@gmail.com>
2021-08-01 17:25:12 +0900

    feedback

--- AkioSanTask6/ViewController.swift
+++ AkioSanTask6/ViewController.swift
@@ -12,48 +12,51 @@ class ViewController: UIViewController {
     @IBOutlet private var randomNumberLabel: UILabel!
     
     @IBOutlet private var slider: UISlider!
-    
+
+    private var randomNumber = 0
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        slider.minimumValue = 1
+        slider.maximumValue = 100
+
+        randomNumber = makeRandomNumber()
         
-        randomNumberLabel.text = "\(round(Float.random(in: 1...100)))"
-        
+        randomNumberLabel.text = "\(randomNumber)"
     }
-    private func dialog(decision: String, value: String) {
+
+    private func makeRandomNumber() -> Int {
+        Int.random(in: 1...100)
+    }
+
+    private func presentDialog(decision: String, value: Int) {
         let alert = UIAlertController(title: "結果",
                                       message: "\(decision)\nあなたの値は: \(value)",
                                       preferredStyle: .alert)
         
-        let action = UIAlertAction(title: "再挑戦", style: .default) { [ weak self ] _ in
-            
-            self?.randomNumberLabel.text = "\(round(Float.random(in: 1...100)))"
-            self?.slider.setValue(50, animated: true)
-        
+        let action = UIAlertAction(title: "再挑戦", style: .default) { [weak self] _ in
+            self?.retry()
         }
         
         alert.addAction(action)
         present(alert, animated: true)
-        
+    }
+
+    private func retry() {
+        randomNumber = makeRandomNumber()
+        randomNumberLabel.text = "\(randomNumber)"
+        slider.setValue(50, animated: true)
     }
     
     @IBAction func decisionButton(_ sender: UIButton) {
-        slider.minimumValue = 1
-        slider.maximumValue = 100
-        
-        let sliderValue = round(slider.value)
 
-        if randomNumberLabel.text == "\(sliderValue)" {
-            
-            dialog(decision: "正解!", value: "\(sliderValue)")
-        
+        let sliderValue = Int(slider.value)
+
+        if sliderValue == randomNumber {
+            presentDialog(decision: "正解!", value: sliderValue)
         } else {
-            
-            dialog(decision: "ハズレ!", value: "\(sliderValue)")
-        
+            presentDialog(decision: "ハズレ!", value: sliderValue)
         }
-        
     }
-    
-    
 }
-
